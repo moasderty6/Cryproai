d9426d2fcc0cafed253218aa3d9a2790813249f1
Naif Chart Crypto Analyzer <m.aldahooh@student.uw.edu.pl>
2026-03-10 12:14:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ NOWPAYMENTS_IPN_SECRET = os.getenv("NOWPAYMENTS_IPN_SECRET")
 DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_USER_ID = 6172153716
 
-GROQ_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
+GROQ_MODEL = "llama-3.3-70b-versatile"
 
 # --- إعداد البوت ---
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
