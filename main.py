0ccf88fd5254b9a2a84452dbe94edf5130c1b6b2
Naif Chart Crypto Analyzer <m.aldahooh@student.uw.edu.pl>
2026-05-30 14:48:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -78,13 +78,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CMC_KEY = os.getenv("CMC_API_KEY")
 BINANCE_API_KEY = "rvApoDI6XRYcki1r2QTnPUBs3QwESzrpTVKohgjbK1zxSzlvrFPxAbZKr94xA2Lx"
-BINANCE_BASES = [
-    "https://api.binance.com",
-    "https://api1.binance.com",
-    "https://api2.binance.com",
-    "https://api3.binance.com",
-    "https://api4.binance.com"
-]
+# 🚀 استبدال القائمة المحظورة بالرابط المحمي عبر Cloudflare
+BINANCE_BASES = ["https://binance-sain.mo-dahoh.workers.dev"]
 
 def get_random_binance_base():
     return random.choice(BINANCE_BASES)
